4c9d7ccb09c63a22a2d99d1c84b212e654c3952a
TingLei-daprediction <leiting2002@gmail.com>
2026-05-15 00:06:51 -0400

    add 3hr verification stuff for base_1 1 month reduced var

---
+++ dd-verify-3hr_leadtime-1month-1hr_refc_input-graphtransformer-base_1-finer_graph_v1-single_input-1month202405-pl925500-reduced_vars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A fv3-cam
+#SBATCH -J anemoi-verify-3h-bg1fgv1m
+#SBATCH -p u1-h100
+#SBATCH -q gpuwf
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=0
+#SBATCH -t 8:00:00
+#SBATCH -o anemoi-gpu-3hrleadtime-1hr_refc_input-graphtransformer-base_1-finer_graph_v1-single_input-1month202405-pl925500-reduced_vars-verify.%j.out
+#SBATCH -e anemoi-gpu-3hrleadtime-1hr_refc_input-graphtransformer-base_1-finer_graph_v1-single_input-1month202405-pl925500-reduced_vars-verify.%j.err
+
+set -euo pipefail
+
+source /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-miniconda3/bin/activate anemoi-training-env-python3.12
+cd /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core
+
+run_id="REPLACE_WITH_RUN_ID"
+training/docs/user-guide/examples/run_rrfs_verify_export_3h_refc_input_graphtransformer_base_1_finer_graph_v1_single_input_1month202405_pl925500_reduced_vars.sh \
+  /scratch3/NCEPDEV/fv3-cam/Ting.Lei/tlei-anemoi-training/base_1_graphtransformer_finer_graph_v1_single_input_1month202405_refc_input_pl925500_reduced_vars/checkpoint/${run_id}/inference-last.ckpt \
+  2024-05-05T00:00:00 2024-05-05T23:00:00 1h
